82421e7411fc00c03993deb4c06b9da67b77e241
hr20281 <83747440+hr20281@users.noreply.github.com>
2021-05-21 18:04:20 +0530

    Add files via upload

---
+++ ConstructorInjection/src/main/java/com/ConstructorInjection/App.java
@@ -0,0 +1,17 @@
+package com.ConstructorInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.Bank;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+    	Bank b = (Bank)context.getBean("bank");
+    	System.out.println(b);
+
+    }
+}

9d38b5cc33696fd527c989dd857304ae6b129b47
hr20281 <83747440+hr20281@users.noreply.github.com>
2021-05-21 18:05:17 +0530

    Add files via upload

---
+++ customdateeditor/src/main/java/com/service/CustomDate.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import java.util.Date;
+
+public class CustomDate {	
+	private Date date;
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}	
+}
